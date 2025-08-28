41949f66a1a381d183ebbdd178f7306d8adf5710
luongdoanh205 <luongvietdoanh2005@gmail.com>
2025-08-28 22:20:36 +0700

    pok

---
+++ LVD3/LVD3/Controllers/MyNewController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LVD3.Controllers
+{
+    public class MyNewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sample()
+        {
+            return View();
+        }
+    }
+}
