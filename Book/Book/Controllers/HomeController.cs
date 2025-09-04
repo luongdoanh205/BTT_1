b5c96591349791d41c0f71299ad2df68758ba205
luongdoanh205 <luongvietdoanh2005@gmail.com>
2025-09-04 18:09:26 +0700

    add

---
+++ Book/Book/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Book.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Tạm dữ liệu cứng, thực tế sẽ lấy từ database
+            var products = new List<string>
+            {
+                "Nồi cơm điện Nagakawa NAG0102",
+                "Nồi cơm điện Sharp KS-TH18",
+                "Nồi cơm điện Panasonic SR-PX184"
+            };
+
+            return View(products);
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Book/Book/ViewComponent/HotProductViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Book.ViewComponents
+{
+    public class HotProductViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var hotProducts = new List<string>
+            {
+                "Nồi cơm điện Nagakawa NAG0102",
+                "Nồi cơm điện Sharp KS-TH18"
+            };
+
+            return View(hotProducts); // => tìm đến Default.cshtml
+        }
+    }
+}
