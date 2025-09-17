f69588d9e044ebf4cee3cd20b3e129e8b3b07624
luongdoanh205 <luongvietdoanh2005@gmail.com>
2025-09-17 20:53:25 +0700

    btt3

---
+++ BTT3/BTT3/Views/Shared/_Scripts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BTT3.Views.Shared
+{
+    public class _ScriptsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
