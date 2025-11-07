4337ddaf47ecde2cce51363440efa4ca509c7506
luongdoanh205 <luongvietdoanh2005@gmail.com>
2025-11-07 18:38:39 +0700

    12

---
+++ D12TH_1/D12TH_1/Models/Gender.cs
@@ -0,0 +1,8 @@
+﻿namespace D12TH_1.Models
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ D12TH_1/D12TH_1/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿
+namespace D12TH_1.Models;
+public class Student
+{
+    public int Id { get; set; } // Mã sinh viên
+    public string? Name { get; set; } // Họ tên
+    public string? Email { get; set; } // Email
+    public string? Password { get; set; } // Mật khẩu
+    public Branch? Branch { get; set; } // Ngành học
+    public Gender? Gender { get; set; } // Giới tính
+    public bool IsRegular { get; set; } // Hệ: true – chính quy, false – phi CQ
+    public string? Address { get; set; } // Địa chỉ
+    public DateTime DateOfBirth { get; set; } // Ngày sinh
+}
