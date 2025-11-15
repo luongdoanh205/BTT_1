ce59692888d10ef425a5518eadac8dd795640138
luongdoanh205 <luongvietdoanh2005@gmail.com>
2025-11-15 21:45:46 +0700

    14

---
+++ DAY13_2/DAY13_1/ViewComponent/MajorViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using DAY13_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DAY13_1.ViewComponent
+{
+    public class MajorViewComponent : ViewComponent
+    {
+        SchoolContext db;
+        List<Major> majors;
+
+        public MajorViewComponent(SchoolContext _context)
+        {
+            db = _context;
+            majors = db.Majors.ToList();
+        }
+
+        public async Task<IViewComponentResult> InvokeAsys()
+        {
+            return View("RenderMajor", majors);
+        }
+    }
+}
